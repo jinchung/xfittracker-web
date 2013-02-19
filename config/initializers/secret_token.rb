2d7d87e517c990dd806f3bc9d32805494b355971
Jin Chung <hyojin.chung17@gmail.com>
2013-02-19 11:16:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XfittrackerWeb::Application.config.secret_token = '65058e5595f237adf669a181b7a07decd9658fc9df99aa93fd392fdccb7c99449097c087c7d96934a3933d54bb7b4c885a6974bf170ac9f7230ee2ed3b21cd95'
